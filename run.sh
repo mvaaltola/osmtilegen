9f760edd3a6fbfe347411097b778905f107fe59e
Mikael Vaaltola <mvaaltola@gmail.com>
2019-07-10 17:29:33 +0300

    Use osmtilegen private key

--- run.sh
+++ run.sh
@@ -6,21 +6,22 @@ echo "Requesting spot instance..."
 
 HOSTNAME=$(python3 request.py)
 
-while ! nc -z $HOSTNAME 22; do   
+# check ssh port 22
+while ! nc -z $HOSTNAME 22; do
   sleep 0.5 # wait for half a second before check again
 done
 
-scp -oStrictHostKeyChecking=no gen-tiles.py 'ubuntu@'$HOSTNAME:/tmp/gen-tiles.py
+scp -oStrictHostKeyChecking=no -i ~/.ssh/osmtilegen.pem gen-tiles.py 'ubuntu@'$HOSTNAME:/tmp/gen-tiles.py
 
-scp -oStrictHostKeyChecking=no mapgen.sh 'ubuntu@'$HOSTNAME:/home/ubuntu
+scp -oStrictHostKeyChecking=no -i ~/.ssh/osmtilegen.pem mapgen.sh 'ubuntu@'$HOSTNAME:/home/ubuntu
 
 echo "Generating maps..."
 
-ssh -oStrictHostKeyChecking=no -oServerAliveInterval=100 'ubuntu@'$HOSTNAME "nohup bash /home/ubuntu/mapgen.sh > mapgen.out 2> mapgen.err < /dev/null &"
+ssh -oStrictHostKeyChecking=no -i ~/.ssh/osmtilegen.pem -oServerAliveInterval=100 'ubuntu@'$HOSTNAME "nohup bash /home/ubuntu/mapgen.sh > mapgen.out 2> mapgen.err < /dev/null &"
 
 echo "Waiting for map generation to complete..."
 
-while ! ssh -oStrictHostKeyChecking=no 'ubuntu@'$HOSTNAME test -f /tmp/tiles.tar; do
+while ! ssh -oStrictHostKeyChecking=no -i ~/.ssh/osmtilegen.pem 'ubuntu@'$HOSTNAME test -f /tmp/tiles.tar; do
     sleep 30
 done
 
@@ -28,10 +29,10 @@ sleep 30
 
 echo "Copying data to local disk..."
 
-scp -oStrictHostKeyChecking=no 'ubuntu@'$HOSTNAME:/tmp/tiles.tar .
+scp -oStrictHostKeyChecking=no -i ~/.ssh/osmtilegen.pem 'ubuntu@'$HOSTNAME:/tmp/tiles.tar .
 
 echo "Done. Shutting down remote host..."
 
-ssh -oStrictHostKeyChecking=no 'ubuntu@'$HOSTNAME sudo shutdown -h now
+ssh -oStrictHostKeyChecking=no -i ~/.ssh/osmtilegen.pem 'ubuntu@'$HOSTNAME sudo shutdown -h now
 
 echo "Map generation finished."

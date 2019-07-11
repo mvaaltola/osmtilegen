be4e012e13fc5f3455e4c5f8bf7f6d317f67ee4c
Mikael Vaaltola <mvaaltola@gmail.com>
2019-07-11 14:41:02 +0300

    Tweak cache size, number of processes

--- mapgen.sh
+++ mapgen.sh
@@ -95,7 +95,7 @@ sudo apt-get install -y mapnik-utils
 
 ./scripts/get-shapefiles.py
 
-sudo su - postgres -c "cd /tmp/openstreetmap-carto; osm2pgsql -G --hstore --style openstreetmap-carto.style --tag-transform-script openstreetmap-carto.lua --cache 20000 --number-processes 20 -d gis ../planet-latest.osm.pbf"
+sudo su - postgres -c "cd /tmp/openstreetmap-carto; osm2pgsql -G --hstore --style openstreetmap-carto.style --tag-transform-script openstreetmap-carto.lua --cache 22000 --number-processes 16 -d gis ../planet-latest.osm.pbf"
 
 sudo -u postgres psql --username=postgres --dbname=gis -f indexes.sql
 
